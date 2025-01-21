82898a8141c46d0b21e909a8e3f61401ff7d8510
Niket Sathe <niketsathe777@gmail.com>
2025-01-21 01:12:21 +0000

    Add new file for <purpose or description>

--- cypress/e2e/performance-test.cy.js
+++ cypress/e2e/performance-test.cy.js
@@ -9,18 +9,10 @@ describe('Add 1000 Todos', () => {
   
       // Loop to add 1000 todos
       for (let i = 1; i <= 1000; i++) {
-        // Type the todo text into the input field
-        //cy.get('input[placeholder="Add a new task"]') // Modify this selector based on your input field
-        //  .type(`${todoText} ${i}`)
-  
-        // Click the 'Add' button (replace the selector as per your app)
-        //cy.get('button[type="submit"]') // Modify this if the button has a different selector
-        //  .click()
+        
 
           cy.addTodo(`${todoText} ${i}`);
-  
-        // Optionally, you can add a short delay to simulate real user interactions
-        // cy.wait(100) // This can be removed if you want to add todos without delay
+
       }
   
       // Verify if 1000 todos are visible (or however you want to check)
